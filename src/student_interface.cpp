eea57170a2e1569d79813d7edaaaf83c3fa6500c
Luca De Menego <lucademenego99@gmail.com>
2021-12-27 18:52:29 +0100

    Find a valid destination point, set correct offset for pol. offsetting

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -13,6 +13,44 @@
 
 namespace student
 {
+
+  /**
+   * @brief Check if a certain points (x,y) is inside the arena
+   * Points are in the order bottom-left, bottom-right, top-right, top-left
+   * 
+   * @param borderPoints Points representing the arena, in order bottom-left, bottom-right, top-right, top-left
+   * @param xPoint X of the point we are considering
+   * @param yPoint Y of the point we are considering
+   * @return true If the point lies inside the arena
+   * @return false If the point does not lie inside the arena
+   */
+  bool isInsideArena(std::vector<visgraph::Point> borderPoints, double xPoint, double yPoint) {
+    return (xPoint >= borderPoints[0].x && xPoint <= borderPoints[1].x && yPoint >= borderPoints[0].y && yPoint <= borderPoints[2].y);
+  }
+
+  /**
+   * @brief Given the coordinates of the destination square, find a point that lies within the arena
+   * 
+   * @param borderPoints Points representing the arena, in order bottom-left, bottom-right, top-right, top-left
+   * @param minX Minimum x of the destination square
+   * @param maxX Maximum x of the destination square
+   * @param minY Minimum y of the destination square
+   * @param maxY Maximum y of the destination square
+   * @return visgraph::Point A point that lies within the arena and that is on an edge of the destination square
+   */
+  visgraph::Point findValidDestinationPoint(std::vector<visgraph::Point> borderPoints, double minX, double maxX, double minY, double maxY) {
+    if (isInsideArena(borderPoints, (minX+maxX)/2.0, minY))
+      return visgraph::Point((minX+maxX)/2.0, minY);
+    if (isInsideArena(borderPoints, (minX+maxX)/2.0, maxY))
+      return visgraph::Point((minX+maxX)/2.0, maxY);
+    if (isInsideArena(borderPoints, maxX, (minY+maxY)/2.0))
+      return visgraph::Point(maxX, (minY+maxY)/2.0);
+    if (isInsideArena(borderPoints, minX, (minY+maxY)/2.0))
+      return visgraph::Point(minX, (minY+maxY)/2.0);
+    
+    return visgraph::Point(-1,-1);
+  }
+
   /**
    * @brief Plan a safe and fast path in the arena
    * 
@@ -32,9 +70,9 @@ namespace student
     std::vector<std::vector<visgraph::Point>> polygons, polygonsForVisgraph;
 
     // TODO: find the correct bound k and the inter size of the dubins
-    double max_k = 10, size = 0.005;
+    double max_k = 10, size = 0.0001;
     // TODO: find the correct offset based on the robot's size for polygon offsetting
-    double offset = 0.065, variant = 10.0;
+    double offset = 0.076, variant = 20.0;
 
     // Find the border's points
     double borderMaxX = -INFINITY, borderMinX = INFINITY;
@@ -64,6 +102,37 @@ namespace student
       polygons.push_back(pol);
     }
 
+    // DEBUG - create the original graph
+    visgraph::Graph originalGraphFirst = visgraph::Graph(polygons, false, true);
+
+    // POLYGON OFFSETTING AND JOIN
+    std::vector<std::vector<std::vector<visgraph::Point>>> pols = enlargeAndJoinObstacles(polygons, offset);
+    polygonsForVisgraph = pols[0];
+    polygons = pols[1];
+
+    // Add borders for collision detection
+    std::vector<visgraph::Point> borderPoints;
+    pol.clear();
+    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
+    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
+    borderPoints.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
+    polygons.push_back(pol);
+    pol.clear();
+    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
+    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
+    borderPoints.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
+    polygons.push_back(pol);
+    pol.clear();
+    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
+    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
+    borderPoints.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
+    polygons.push_back(pol);
+    pol.clear();
+    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
+    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
+    borderPoints.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
+    polygons.push_back(pol);
+
     // Set origin
     visgraph::Point origin = visgraph::Point(x[0], y[0]);
 
@@ -84,35 +153,12 @@ namespace student
         minY = gate_list[0][i].y;
       }
     }
-    /****************IMPORTANT: remember to remove the 0.1 if it works on your machine!!!*************/
-    visgraph::Point destination = visgraph::Point((maxX + minX) / 2.0, (maxY + minY) / 2.0 - 0.1); 
 
-    // DEBUG - create the original graph
-    visgraph::Graph originalGraphFirst = visgraph::Graph(polygons, false, true);
-
-    // POLYGON OFFSETTING AND JOIN
-    std::vector<std::vector<std::vector<visgraph::Point>>> pols = enlargeAndJoinObstacles(polygons, offset);
-    polygonsForVisgraph = pols[0];
-    polygons = pols[1];
-
-    // Add borders for collision detection
-    pol.clear();
-    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
-    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
-    polygons.push_back(pol);
-    pol.clear();
-    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
-    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
-    polygons.push_back(pol);
-    pol.clear();
-    pol.push_back(visgraph::Point(borderMaxX-(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
-    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
-    polygons.push_back(pol);
-    pol.clear();
-    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMaxY-(offset + (offset / variant))));
-    pol.push_back(visgraph::Point(borderMinX+(offset + (offset / variant)), borderMinY+(offset + (offset / variant))));
-    polygons.push_back(pol);
-
+    visgraph::Point destination = findValidDestinationPoint(borderPoints, minX, maxX, minY, maxY);
+    if (destination == visgraph::Point(-1, -1)) {
+      std::cout << "UNABLE TO DETERMINE A VALID DESTINATION POINT\n";
+      return false;
+    }
 
     // COMPUTE VISIBILITY GRAPH
     visgraph::VisGraph visg;
@@ -122,9 +168,10 @@ namespace student
     // COMPUTE SHORTEST PATH
     std::vector<visgraph::Point> shortestPath = g.shortestPath(origin, destination);
 
-    printGraph(originalGraphFirst.graph, origin, destination, shortestPath);
-    printGraph(originalGraph.graph, origin, destination, shortestPath);
-    printGraph(g.graph, origin, destination, shortestPath);
+    // DEBUG
+    // printGraph(originalGraphFirst.graph, origin, destination, shortestPath);
+    // printGraph(originalGraph.graph, origin, destination, shortestPath);
+    // printGraph(g.graph, origin, destination, shortestPath);
 
     // COMPUTE MULTIPOINT DUBINS SHORTEST PATH
     dubins::Dubins dubins = dubins::Dubins(max_k, size);
@@ -134,41 +181,43 @@ namespace student
         points[i] = new dubins::DubinsPoint(shortestPath[i].x, shortestPath[i].y);
     }
     points[shortestPath.size()-1] = new dubins::DubinsPoint(shortestPath[shortestPath.size()-1].x, shortestPath[shortestPath.size()-1].y);
-
     dubins::DubinsCurve **curves = dubins.multipointShortestPath(points, shortestPath.size(), originalGraph);
     if (curves == nullptr) {
         std::cout << "UNABLE TO COMPUTE A PATH FOR GIVEN INPUT\n";
+        // return false;
     } else {
         std::cout << "COMPLETED MULTIPOINT SHORTEST PATH SUCCESSFULLY\n";
-        dubins.printCompletePath(curves, shortestPath.size()-1, polygons);
+        // DEBUG
+        // dubins.printCompletePath(curves, shortestPath.size()-1, polygons);
+
+      for(int n = 0; n < shortestPath.size()-1; n++) {
+        dubins::DubinsCurve *result = curves[n];
+        int npts = result->a1->L/size;
+
+        for (int i = 0; i < npts; i++) {
+          double s = result->a1->L/npts * i;
+          dubins::DubinsLine *tmp = new dubins::DubinsLine(s, result->a1->x0, result->a1->y0, result->a1->th0, result->a1->k);
+          path[0].points.emplace_back(s, tmp->x, tmp->y, tmp->th, result->a1->k);
+          delete tmp;
+        }
+
+        npts = result->a2->L/size;
+        for (int i = 0; i < npts; i++) {
+          double s = result->a2->L/npts * i;
+          dubins::DubinsLine *tmp = new dubins::DubinsLine(s, result->a2->x0, result->a2->y0, result->a2->th0, result->a2->k);
+          path[0].points.emplace_back(s, tmp->x, tmp->y, tmp->th, result->a2->k);
+          delete tmp;
+        }
+
+        npts = result->a3->L/size;
+        for (int i = 0; i < npts; i++) {
+          double s = result->a3->L/npts * i;
+          dubins::DubinsLine *tmp = new dubins::DubinsLine(s, result->a3->x0, result->a3->y0, result->a3->th0, result->a3->k);
+          path[0].points.emplace_back(s, tmp->x, tmp->y, tmp->th, result->a3->k);
+          delete tmp;
+        }
+      }
     }
-
-    for(int n = 0; n < shortestPath.size()-1; n++) {
-      dubins::DubinsCurve *result = curves[n];
-      int npts = result->a1->L/size;
-
-      for (int i = 0; i < npts; i++) {
-        double s = result->a1->L/npts * i;
-        dubins::DubinsLine *tmp = new dubins::DubinsLine(s, result->a1->x0, result->a1->y0, result->a1->th0, result->a1->k);
-        path[0].points.emplace_back(s, tmp->x, tmp->y, tmp->th, result->a1->k);
-        delete tmp;
-      }
-
-      npts = result->a2->L/size;
-      for (int i = 0; i < npts; i++) {
-        double s = result->a2->L/npts * i;
-        dubins::DubinsLine *tmp = new dubins::DubinsLine(s, result->a2->x0, result->a2->y0, result->a2->th0, result->a2->k);
-        path[0].points.emplace_back(s, tmp->x, tmp->y, tmp->th, result->a2->k);
-        delete tmp;
-      }
-
-      npts = result->a3->L/size;
-      for (int i = 0; i < npts; i++) {
-        double s = result->a3->L/npts * i;
-        dubins::DubinsLine *tmp = new dubins::DubinsLine(s, result->a3->x0, result->a3->y0, result->a3->th0, result->a3->k);
-        path[0].points.emplace_back(s, tmp->x, tmp->y, tmp->th, result->a3->k);
-        delete tmp;
-      }
-    }
+    return true;
   }
 }
